f73a1ecd22732a496b2c632b3cc05d24b5b11ca6
DaVaR <davar@thedavar.net>
2015-07-03 23:09:01 -0500

    Updates
    Updated site name and url throughout the site.

--- index.php
+++ index.php
@@ -31,7 +31,6 @@ $userId = $get_loggedin_uid;
 
 require("models/members/funcs_user_info.inc");
 require("models/functions.inc");
-require("models/sublinks.inc");
 require("models/funcs_styles.inc");
 
 // Run the page handler
