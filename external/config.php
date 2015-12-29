3325088451f4f7ecbff49a05f3bac47afaea25d3
DaVaR <davar@thedavar.net>
2015-12-28 20:09:59 -0600

    Fix error
    Fixed undefined page error.

--- external/config.php
+++ external/config.php
@@ -92,7 +92,9 @@ if(isset($_SESSION["userCakeUser"]) && is_object($_SESSION["userCakeUser"]))
 }
 
 // Build Security for Current Page if Any
-$cur_page_get = "pages/".$_GET['page'].".php";
-if (!securePage($cur_page_get)){die();}
+if(isset($_GET['page'])){
+	$cur_page_get = "pages/".$_GET['page'].".php";
+	if (!securePage($cur_page_get)){die();}
+}
 
 ?>
